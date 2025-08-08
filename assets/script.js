e2a15fe3422847c0c358e11f3be4bf8f8a1ebc29
Rahul Gomes <rahulgomes19@gmail.com>
2025-08-07 23:36:36 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -167,7 +167,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 
-async function loadResearch() {
+async function loadGrants() {
   const bodyId = "grants";
   const searchId = "gSearch";
 
@@ -224,7 +224,7 @@ async function loadResearch() {
         case "index.html":         loadHome(); break;
         case "publications.html":  loadPublications(); break;
         case "about.html":         loadAbout(); break;
-        case "grants.html":        loadResearch(); break;
+        case "grants.html":        loadGrants(); break;
         default:
             // We'll keep adding loaders for the rest of the pages as we go.
             console.log(`No loader defined yet for ${page}`);
